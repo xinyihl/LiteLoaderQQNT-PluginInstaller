42b79fba5dbcfbfa7bf76e577dd910bf05e91b79
xinyihl <1012737146@qq.com>
2024-04-26 18:31:54 +0800

    docs card

--- docs/main.js
+++ docs/main.js
@@ -19,13 +19,31 @@ async function init() {
     plugins
       .filter((notNull) => notNull)
       .forEach((plugin) => {
+        const icon = plugin.icon ? `https://raw.githubusercontent.com/${plugin.repository.repo}/${plugin.repository.branch}/${plugin.icon}` : "default_icon.png"
         const temp = `
-      <div>
-          <h3 style="display: unset;">${plugin.name}</h3><br>
-          <span>${plugin.version}</span>
-          <p style="display: unset;">${plugin.description}</p>
-          <a href="llqqnt://plugininstaller/${plugin.repository.repo}/${plugin.repository.branch}/manifest.json">安装</a>
-      </div>
+        <div class="plugin-card">
+            <div class="desc">
+              <img  src="${icon}" class="icon-common icon" />
+              <div class="icon-common class-6">
+                <span class="class-4">${plugin.name}</span
+                ><span class="class-5"
+                  >${plugin.description}</span
+                >
+              </div>
+            </div>
+            <div class="info">
+              <div class="class-11">
+                <span class="class-7">版本：</span><span class="class-9">${plugin.version}</span><span class="class-8">作者：</span
+                ><a href="${plugin.authors[0].link}" class="class-10">${plugin.authors[0].name}</a>
+              </div>
+              <div class="class-12">
+                <span class="class-13">支持平台：</span><span class="class-14">${plugin.platform}</span>
+              </div>
+              <div class="class-17"><span class="class-16">加载器版本：</span><span class="class-18">${plugin.manifest_version}</span></div>
+              <a href="llqqnt://plugininstaller/${plugin.repository.repo}/${plugin.repository.branch}/manifest.json" class="install">安装</a>
+            </div>
+          </div>
+        </div>
       `;
         const doc = new DOMParser().parseFromString(temp, "text/html");
         document.querySelector(".app").appendChild(doc.querySelector("div"));
