911c32395d04eaaf13ca4dd16b417d84853a3c20
xinyihl <1012737146@qq.com>
2024-05-06 21:40:59 +0800

    docs

--- src/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("plugininstaller", {
-    log: (level, ...serializedArgs) => ipcRenderer.send("LiteLoader.plugininstaller.log", level, ...serializedArgs),
-    openWeb: (url) => ipcRenderer.send("LiteLoader.plugininstaller.openWeb", url),
-    installByUrl: (url) => ipcRenderer.send("LiteLoader.plugininstaller.installByUrl", url),
-    updateBySlug: (slug) => ipcRenderer.send("LiteLoader.plugininstaller.updateBySlug", slug),
-    chackPluginUpdate: (slug) => ipcRenderer.invoke("LiteLoader.plugininstaller.chackPluginUpdate", slug)
-});
