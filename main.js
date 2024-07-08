d99a61bdfadef11ae84f677bb839dde08f2e9518
xinyihl <1012737146@qq.com>
2024-07-08 15:07:43 +0800

    fix some bug

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const closelists = ["qqsss"]
+
 async function init() {
   const app = document.querySelector(".app");
   const searchParams = new URLSearchParams(window.location.search);
@@ -30,35 +32,42 @@ async function init() {
     plugins
       .filter((notNull) => notNull)
       .forEach((plugin) => {
-        if (!plugin.repository) return;
-        const icon = plugin.icon ? `https://raw.githubusercontent.com/${plugin.repository.repo}/${plugin.repository.branch}/${plugin.icon}` : "default_icon.png"
-        const temp = `
-      <div class="plugin-card">
-        <div class="desc">
-          <img src="${icon}" class="icon" />
-          <div>
-            <span class="name">${plugin.name}</span>
-            <span class="description">${plugin.description}</span>
+        try{
+          const icon = plugin.icon ? `https://raw.githubusercontent.com/${plugin.repository.repo}/${plugin.repository.branch}/${plugin.icon}` : "default_icon.png"
+          var text = ''
+          if(closelists.includes(plugin.slug)){
+            text = `<span style="float: right; color: red;" title="该插件为闭源/混淆插件，请谨慎使用">闭源</span>`
+          }
+          const temp = `
+          <div class="plugin-card">
+            <div class="desc">
+              <img src="${icon}" class="icon" />
+              <div>
+                <span class="name">${plugin.name + text}</span>
+                <span class="description">${plugin.description}</span>
+              </div>
+            </div>
+            <div class="info">
+              <div>
+                <span class="version">版本：</span><span>${plugin.version}</span>
+                <span class="author">作者：</span><a href="${plugin.authors[0].link}" title="点击打开 ${plugin.authors[0].name} 作者首页" >${plugin.authors[0].name}</a>
+              </div>
+              <div>
+                <span>支持平台：</span><span>${plugin.platform}</span>
+              </div>
+              <div>
+                <span>加载器版本：</span><span>${plugin.manifest_version}</span>
+              </div>
+              <a href="llqqnt://plugininstaller/${plugin.repository.repo}/${plugin.repository.branch}/manifest.json" title="点击安装 ${plugin.name}" class="link">安装</a>
+              <a href="https://github.com/${plugin.repository.repo}" title="点击打开 ${plugin.name} 仓库" class="link">详情</a>
+            </div>
           </div>
-        </div>
-        <div class="info">
-          <div>
-            <span class="version">版本：</span><span>${plugin.version}</span>
-            <span class="author">作者：</span><a href="${plugin.authors[0].link}" title="点击打开 ${plugin.authors[0].name} 作者首页" >${plugin.authors[0].name}</a>
-          </div>
-          <div>
-            <span>支持平台：</span><span>${plugin.platform}</span>
-          </div>
-          <div>
-            <span>加载器版本：</span><span>${plugin.manifest_version}</span>
-          </div>
-          <a href="llqqnt://plugininstaller/${plugin.repository.repo}/${plugin.repository.branch}/manifest.json" title="点击安装 ${plugin.name}" class="link">安装</a>
-          <a href="https://github.com/${plugin.repository.repo}" title="点击打开 ${plugin.name} 仓库" class="link">详情</a>
-        </div>
-      </div>
-      `;
-        const doc = new DOMParser().parseFromString(temp, "text/html");
-        app.appendChild(doc.querySelector("div"));
+          `;
+          const doc = new DOMParser().parseFromString(temp, "text/html");
+          app.appendChild(doc.querySelector("div"));
+        }catch (e) {
+          console.error(plugin, e);
+        }
       });
   });
 }
